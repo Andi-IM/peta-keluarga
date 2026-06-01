9e59e362e4af64bfd538db23ba6f27fa37221d2e
rahmatdenas <120230537+rahmatdenas@users.noreply.github.com>
2026-06-01 12:42:36 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  output: 'export', 
+
   typescript: {
+
     ignoreBuildErrors: true,
+
   },
+
   images: {
+
     unoptimized: true,
+
   },
+
 }
+
+
 
 export default nextConfig
